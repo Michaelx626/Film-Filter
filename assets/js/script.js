f7041747350bc2da4c35b1bd09501a96ad39021a
Michaelx626 <118804354+Michaelx626@users.noreply.github.com>
2023-01-24 21:53:35 -0800

    Day 3 Commits

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,27 @@ var APIKey = "c646bc26";
 var youtubeKey = "AIzaSyCtFE4ZvsoVix-qoD9T5Le6AAIkvG3l-Dg";
 var userSearch = document.getElementById("search");
 var searchButton = document.getElementById("search-button");
-var youtubeHTTP = 'https://www.youtube.com/watch?v=';
-var movieTitle = document.getElementById('movie-title');
-var movieYears = document.getElementById('movie-year');
-var movieContainer = document.getElementById('movie-container');
+var youtubeHTTP = "https://www.youtube.com/watch?v=";
+var movieTitle = document.getElementById("movie-title");
+var movieYears = document.getElementById("movie-year");
+var movieSection = document.querySelector(".movie-container");
 
 function runProgram() {
   var userInput = userSearch.value.trim();
-  var currentUrl = "http://www.omdbapi.com/?s=" + userInput + "&apikey=" + APIKey;
-//   var youtubeUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=rating&q=' + userInput + '&type=video&key=' + youtubeKey;
+  var currentUrl =
+    "http://www.omdbapi.com/?s=" + userInput + "&apikey=" + APIKey;
+  var youtubeUrl =
+    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=rating&q=" +
+    userInput +
+    "%20official%20trailer&type=video&key=" +
+    youtubeKey;
+  // add a modal => inject the frame of the youtube video
+  // give the div an id for the movie containers
+  // event listener for all movies; querySelectorAll('...')
+  // trigger the modal dialog => add in the content will be dynamic => make the call to youtube with the youtube ID (after)
+  // extract the youtube title and append it to URL
+  // iFrame => source => youtube http + videoID
+
   fetch(currentUrl)
     .then(function (response) {
       return response.json();
@@ -21,32 +33,43 @@ function runProgram() {
       var titles = [];
       var years = [];
       var poster = [];
-      for (var i = 0; i < movies.length; i++){
+      for (var i = 0; i < movies.length; i++) {
         var filteredTitles = movies[i].Title;
         var filteredYears = movies[i].Year;
         var filteredPoster = movies[i].Poster;
         titles.push(filteredTitles);
         years.push(filteredYears);
         poster.push(filteredPoster);
-        var createTitle = document.createElement('h2');
-        var createYear = document.createElement('h4');
-        var createPoster = document.createElement('img');
-        createPoster.setAttribute('src', poster[i]);
+        var movieContainer = document.createElement("div");
+        var createTitle = document.createElement("a");
+        var createYear = document.createElement("h4");
+        var createPoster = document.createElement("img");
+        var favoriteButton = document.createElement('button');
+        favoriteButton.setAttribute('type', 'submit');
+        favoriteButton.textContent = "Add to Favorites";
+        favoriteButton.addEventListener('click', function(){
+            // localStorage.setItem('movie', title[i]);
+        })
+        createPoster.setAttribute("src", poster[i]);
+        createTitle.setAttribute("href", "#");
+        createTitle.setAttribute("data-attribute", titles[i]);
+        createTitle.addEventListener("click", function (event) {
+          event.target.getAttribute("data-attribute") == titles[i];
+          fetch(youtubeUrl)
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (data) {
+              console.log(data);
+                // collect the videoIDs
+            });
+        });
         createTitle.innerHTML = titles[i];
         createYear.innerHTML = years[i];
-        movieContainer.appendChild(createTitle);
-        movieContainer.appendChild(createYear);
-        movieContainer.appendChild(createPoster);
+        movieContainer.append(createTitle, createYear, createPoster, favoriteButton);
+        movieSection.appendChild(movieContainer);
       }
     });
-
-    // fetch(youtubeUrl)
-    //     .then(function (response){
-    //         return response.json()
-    //     })
-    //     .then(function (data){
-    //         console.log(data);
-    //     })
 }
 
 searchButton.addEventListener("click", runProgram);
