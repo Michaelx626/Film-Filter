f1aee7d3a5e44da0709086dcf51f10faec0f8328
Michaelx626 <118804354+Michaelx626@users.noreply.github.com>
2023-01-30 15:36:18 -0800

    changes made to files look more clean

---
+++ assets/README.md
@@ -0,0 +1,21 @@
+ <h1 align="center"> Film Filter </h1>
+
+<h2 align="center"> Project Description </h2>
+<p align="center"> This project will help users search for his or her movies based on their inputs. The movie data will be retrieved from OMDB Api displaying the title name, year of the movie, and an picture of the movie. If the user chooses to click on the title name then a top 5 list of official trailers taken from YouTube will be displayed on the side. The YouTube data will be consist of the movie title trailer and an actual URL link to that video on a separate open tab. The user is also allowed to save his or her favorite movies. They will be displayed on the favorites button to the left and the user is allowed to delete the favorites whenever he or she pleases. This project helped my colleagues and I understand the importance about server APIs and how it can fetch specific data that we need to display to the user to provide a better experience. </p>
+  
+<h2 align="center"> Installation </h2>
+<p align="center"> N/A </p>
+
+<h2 align="center"> Usage </h2>
+<p align="center"> You can access my project, Film Filter, by simply clicking on this highlighted text. </p>
+<p align="center"> <a href="TBA..." target="_blank"> Here </a> </p>
+
+<h2 align="center"> Screenshots </h2>
+
+![text](TBA..)
+
+<h2 align="center">  Credit </h2>
+<p align="center"> N/A
+
+<h2 align="center"> Licenses </h2>
+<p align="center"> Copyright (c) 2023 Michael Lin, Tyler Gosley, and Regan Wilson </p>
--- assets/js/script.js
+++ assets/js/script.js
@@ -39,131 +39,132 @@ function callYoutube (event) {
 function fetchMovieData(data){
   console.log(data);
   var movies = data.Search;
-    var titles = [];
-    var years = [];
-    var poster = [];
-    deleteExisitingContent();
-    for (var i = 0; i < movies.length; i++) {
-      var filteredTitles = movies[i].Title;
-      var filteredYears = movies[i].Year;
-      var filteredPoster = movies[i].Poster;
+  var titles = [];
+  var years = [];
+  var poster = [];
+  deleteExisitingContent();
+  for (var i = 0; i < movies.length; i++) {
+    var filteredTitles = movies[i].Title;
+    var filteredYears = movies[i].Year;
+    var filteredPoster = movies[i].Poster;
       
-      titles.push(filteredTitles);
-      years.push(filteredYears);
-      poster.push(filteredPoster);
+    titles.push(filteredTitles);
+    years.push(filteredYears);
+    poster.push(filteredPoster);
       
-      var movieContainer = document.createElement("div");
-      var createTitle = document.createElement("a");
-      var createYear = document.createElement("h4");
-      var createPoster = document.createElement("img");
-      var favoriteButton = document.createElement('button');
+    var movieContainer = document.createElement("div");
+    var createTitle = document.createElement("a");
+    var createYear = document.createElement("h4");
+    var createPoster = document.createElement("img");
+    var favoriteButton = document.createElement('button');
       
-      favoriteButton.setAttribute('type', 'submit');
-      movieContainer.setAttribute('class', 'movieResults');
-      favoriteButton.setAttribute('data-attribute', titles[i]);
-      createTitle.setAttribute("href", "#");
-      createTitle.setAttribute("data-attribute", titles[i]);
+    favoriteButton.setAttribute('type', 'submit');
+    movieContainer.setAttribute('class', 'movieResults');
+    favoriteButton.setAttribute('data-attribute', titles[i]);
+    createTitle.setAttribute("href", "#");
+    createTitle.setAttribute("data-attribute", titles[i]);
       
-      if (poster[i] !== "N/A"){
-        createPoster.setAttribute("src", poster[i]);
-      } else {
-        createPoster.setAttribute("src", "https://demofree.sirv.com/nope-not-here.jpg?w=150");
-      }
-      
-      favoriteButton.addEventListener('click', storeFavorites);
-      createTitle.addEventListener("click", callYoutube);
-      
-      favoriteButton.textContent = "Add to Favorites";
-      createTitle.innerHTML = titles[i];
-      createYear.innerHTML = years[i];
-      
-      movieContainer.append(createTitle, createYear, createPoster, favoriteButton);
-      movieSection.appendChild(movieContainer);
+    if (poster[i] !== "N/A"){
+      createPoster.setAttribute("src", poster[i]);
+    } else {
+      createPoster.setAttribute("src", "https://demofree.sirv.com/nope-not-here.jpg?w=150");
     }
+      
+    favoriteButton.addEventListener('click', storeFavorites);
+    createTitle.addEventListener("click", callYoutube);
+      
+    favoriteButton.textContent = "Add to Favorites";
+    createTitle.innerHTML = titles[i];
+    createYear.innerHTML = years[i];
+      
+    movieContainer.append(createTitle, createYear, createPoster, favoriteButton);
+    movieSection.appendChild(movieContainer);
   }
+}
   
-  function fetchYouTubeData(data){
-      console.log(data);
-      var videoIDs = data.items;
-      var youtubeIDs = [];
-      var youtubeResultsTitle = [];
-      for (var j = 0; j < videoIDs.length; j++){
-        var filteredIDs = videoIDs[j].id.videoId;
-        var filteredyoutubeTitles = videoIDs[j].snippet.title;
+function fetchYouTubeData(data){
+  console.log(data);
+  var videoIDs = data.items;
+  var youtubeIDs = [];
+  var youtubeResultsTitle = [];
+  for (var j = 0; j < videoIDs.length; j++){
+    var filteredIDs = videoIDs[j].id.videoId;
+    var filteredyoutubeTitles = videoIDs[j].snippet.title;
   
-        var youtubeContainer = document.createElement('div');
-        var youtubeTitle = document.createElement('h5');
-        var youtubeLink = document.createElement('a');
+    var youtubeContainer = document.createElement('div');
+    var youtubeTitle = document.createElement('h5');
+    var youtubeLink = document.createElement('a');
+    
+    youtubeIDs.push(youtubeHTTP + filteredIDs);
+    youtubeResultsTitle.push(filteredyoutubeTitles);
+    
+    youtubeContainer.setAttribute('class', 'youtubeResults');
+    youtubeLink.setAttribute('href', youtubeIDs[j]);
+    youtubeLink.setAttribute('target', '_blank');
   
-        youtubeContainer.setAttribute('class', 'youtubeResults');
-        youtubeLink.setAttribute('href', youtubeIDs[j]);
-        youtubeLink.setAttribute('target', '_blank');
-  
-        youtubeIDs.push(youtubeHTTP + filteredIDs);
-        youtubeResultsTitle.push(filteredyoutubeTitles);
-  
-        youtubeTitle.innerHTML = youtubeResultsTitle[j];
-        youtubeLink.innerHTML = youtubeIDs[j];
-  
-        youtubeContainer.append(youtubeTitle, youtubeLink);
-        youtubeSection.appendChild(youtubeContainer);
-      }
-  }
-  
-  function displayFaves(event){
-    event.preventDefault();
-    deleteExisitingContent();
-    for (var k = 0; k < userStored.length; k++){
-      var favoriteTitle = document.createElement('a');
-      var deleteButton = document.createElement('button');
-      var favoriteContainer = document.createElement('div');
-      
-      favoriteTitle.setAttribute('data-attribute', userStored[k]);
-      favoriteTitle.setAttribute('href', '#');
-      deleteButton.setAttribute('id', 'delete');
-      deleteButton.setAttribute('data-movie-title', userStored[k]);
-      deleteButton.setAttribute('type', 'submit');
-      favoriteContainer.setAttribute('class', 'fave-container');
-      
-      favoriteTitle.addEventListener('click', callYoutube);
-      deleteButton.addEventListener('click', removeFavorites);
-      
-      deleteButton.textContent = "Delete";
-      favoriteTitle.innerHTML = userStored[k];
-      
-      favoriteContainer.append(favoriteTitle, deleteButton);
-      movieSection.appendChild(favoriteContainer);
-    }
-  }
 
-  function storeFavorites(event){
-    var addFavorite = event.target.getAttribute('data-attribute');
-    userStored.push(addFavorite);
-    setItem();
-  }
+    youtubeTitle.innerHTML = youtubeResultsTitle[j];
+    youtubeLink.innerHTML = youtubeIDs[j];
   
-  function removeFavorites(event){
-    var movieTitle = event.target.getAttribute('data-movie-title');
-    var indexToRemove = userStored.indexOf(movieTitle);
-    userStored.splice(indexToRemove, 1);
-    setItem();
-    displayFaves(event);
+    youtubeContainer.append(youtubeTitle, youtubeLink);
+    youtubeSection.appendChild(youtubeContainer);
   }
+}
   
-  function refreshPage(event){
-    event.preventDefault();
-    deleteExisitingContent();
+function displayFaves(event){
+  event.preventDefault();
+  deleteExisitingContent();
+  for (var k = 0; k < userStored.length; k++){
+    var favoriteTitle = document.createElement('a');
+    var deleteButton = document.createElement('button');
+    var favoriteContainer = document.createElement('div');
+      
+    favoriteTitle.setAttribute('data-attribute', userStored[k]);
+    favoriteTitle.setAttribute('href', '#');
+    deleteButton.setAttribute('id', 'delete');
+    deleteButton.setAttribute('data-movie-title', userStored[k]);
+    deleteButton.setAttribute('type', 'submit');
+    favoriteContainer.setAttribute('class', 'fave-container');
+      
+    favoriteTitle.addEventListener('click', callYoutube);
+    deleteButton.addEventListener('click', removeFavorites);
+      
+    deleteButton.textContent = "Delete";
+    favoriteTitle.innerHTML = userStored[k];
+      
+    favoriteContainer.append(favoriteTitle, deleteButton);
+    movieSection.appendChild(favoriteContainer);
   }
+}
 
-  function deleteExisitingContent (){
-    movieSection.textContent = "";
-    youtubeSection.textContent = "Top 5 YouTube Search Results:";
-  }
+function storeFavorites(event){
+  var addFavorite = event.target.getAttribute('data-attribute');
+  userStored.push(addFavorite);
+  setItem();
+}
   
-  function setItem(){
-    localStorage.setItem('movies', JSON.stringify(userStored));
-  }
+function removeFavorites(event){
+  var movieTitle = event.target.getAttribute('data-movie-title');
+  var indexToRemove = userStored.indexOf(movieTitle);
+  userStored.splice(indexToRemove, 1);
+  setItem();
+  displayFaves(event);
+}
+  
+function refreshPage(event){
+  event.preventDefault();
+  deleteExisitingContent();
+}
 
-  searchButton.addEventListener("click", runProgram);
-  homeButton.addEventListener('click', refreshPage);
-  displayFavorites.addEventListener('click', displayFaves);
+function deleteExisitingContent (){
+  movieSection.textContent = "";
+  youtubeSection.textContent = "Top 5 YouTube Search Results:";
+}
+  
+function setItem(){
+  localStorage.setItem('movies', JSON.stringify(userStored));
+}
+
+searchButton.addEventListener("click", runProgram);
+homeButton.addEventListener('click', refreshPage);
+displayFavorites.addEventListener('click', displayFaves);
